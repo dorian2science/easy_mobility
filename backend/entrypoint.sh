0de554d04a4a243053953ee7be8bbdb0aee71924
Dorian Drevon <drevondorian@gmail.com>
2026-03-16 03:56:50 +0100

    ci deploy changed

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,4 +4,4 @@ cd /app/backend
 alembic upgrade head
 cd /app
 python scripts/seed_db.py
-exec uvicorn backend.api:app --host 0.0.0.0 --port 8000
+exec uvicorn backend.api:app --host 0.0.0.0 --port "${PORT:-8000}"
